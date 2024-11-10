3759d93f678da6d4d297107279b0945d06b9ee13
codinglabe <codinglabe@gmail.com>
2024-11-11 04:00:57 +0600

    ffdfdf

--- frontend/app/payment/(status)/success/page.tsx
+++ frontend/app/payment/(status)/success/page.tsx
@@ -2,12 +2,15 @@
 import MasterLayout from "@/components/layout/MasterLayout";
 import PaymentSuccess from "@/components/payment/PaymentSuccess";
 import "../../../globals.css";
+import { Suspense } from "react";
 
 export default async function Success() {
 
   return (
     <MasterLayout>
-        <PaymentSuccess/>
+        <Suspense fallback={<div>Loading...</div>}>
+          <PaymentSuccess/>
+        </Suspense>
     </MasterLayout>
   )
 }
