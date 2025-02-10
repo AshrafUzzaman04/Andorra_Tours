a1d5607e8a1eb503d039403ede72332aa9748fd4
codinglabe <codinglabe@gmail.com>
2025-02-10 21:56:59 +0600

    what's app button solve

--- frontend/components/sections/WhatsappButton.tsx
+++ frontend/components/sections/WhatsappButton.tsx
@@ -1,7 +1,7 @@
 export default function WhatsappButton() {
     return (
         <>
-            <a href="https://wa.me/34675136764" _target="_blank" className="whatsapp-btn">
+            <a href="https://wa.me/34675136764" target="_blank" className="whatsapp-btn">
                         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/120px-WhatsApp.svg.png" alt="" />
             </a>
         </>
